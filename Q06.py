a6fc7f4142941f1c1e0364a940dbfcdb2e8903fe
Kushal-Raj-R <kushalraj3107@gmail.com>
2026-04-08 20:20:15 +0530

    Update Q06.py

--- Q06.py
+++ Q06.py
@@ -12,3 +12,11 @@
 # Sample Output 2:  0! = 1
 
 # --- YOUR CODE HERE ---
+n = int(input("Enter a number: "))
+fact = 1
+if n == 0:
+  print(f"{n}! = 1")
+else:
+  for i in range(1 , n+1):
+    fact = fact * i
+print(f"{n}! = {fact}")
